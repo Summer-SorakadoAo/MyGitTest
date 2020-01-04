9452cfa255fa85d4b5d3fa6f23752bac2f3fe1b2
Summer-SorakadoAo <sorakadoao_key@163.com>
2020-01-04 19:23:27 +0800

    This is test

---
+++ src/com/yr/MyGit.java
@@ -0,0 +1,12 @@
+package com.yr;
+
+public class MyGit {
+	public static void main(String[] args) {
+		MyGit mg = new MyGit();
+		mg.get();
+	}
+	
+	public void get() {
+		System.out.println("123456");
+	}
+}
