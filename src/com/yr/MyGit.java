7f3eadba64bb79e07aa58d98333079c81b15b5d5
Summer-SorakadoAo <59468055+Summer-SorakadoAo@users.noreply.github.com>
2020-01-04 20:41:20 +0800

    Update MyGit.java

--- src/com/yr/MyGit.java
+++ src/com/yr/MyGit.java
@@ -9,6 +9,8 @@ public class MyGit {
 	public void get() {
 		System.out.println("123456------");
 		System.out.println("2222222222");
+		System.out.println("3333333");
 		System.out.println("4444444");
+		System.out.println("test-Yun");
 	}
 }
